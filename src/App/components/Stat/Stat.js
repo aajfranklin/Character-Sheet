55532639d725a8ac69019e23664015b279adb70c
aajfranklin <aajfranklin@gmail.com>
2019-05-31 11:03:59 +0100

    additional validation for stat updates

--- src/App/components/Stat/Stat.js
+++ src/App/components/Stat/Stat.js
@@ -3,7 +3,18 @@ import {cacheStat, revertStat, toggleShowError, updateStat, updateStatSuccess} f
 import { INVALID_STAT } from '../Error/ErrorTypes';
 import { connect } from 'react-redux';
 
-export function Stat({beingEdited, cacheStat, handleBlur, stat, stats, updateStatLocally}) {
+export function Stat({beingEdited, cacheStat, cachedValue, revertWithError, revertWithoutError, stat, stats, updateStat, updateStatLocally}) {
+
+    const handleBlur = (e) => {
+        const value = e.target.value;
+        if (isNaN(value) || value === '') {
+            revertWithError();
+        } else if (value === cachedValue) {
+            revertWithoutError();
+        } else {
+            updateStat(stat, parseInt(value));
+        }
+    };
 
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') document.activeElement.blur();
@@ -19,21 +30,23 @@ export function Stat({beingEdited, cacheStat, handleBlur, stat, stats, updateSta
 function mapStateToProps(state) {
     return {
         stats: state.app.stats,
-        beingEdited: state.app.statCache.stat
+        beingEdited: state.app.statCache.stat,
+        cachedValue: state.app.statCache.value
     };
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
-        handleBlur: (e) => {
-            const value = e.target.value;
-            if (isNaN(value)|| value === '') {
-                dispatch(revertStat(ownProps.stat));
-                dispatch(toggleShowError(INVALID_STAT));
-            } else {
-                dispatch(updateStat(ownProps.stat, parseInt(value)));
-                dispatch(cacheStat(''));
-            }
+        revertWithError: () => {
+            dispatch(revertStat(ownProps.stat));
+            dispatch(toggleShowError(INVALID_STAT));
+        },
+        revertWithoutError: () => {
+            dispatch(revertStat(ownProps.stat));
+        },
+        updateStat: (stat, value) => {
+            dispatch(updateStat(stat, value));
+            dispatch(cacheStat(''));
         },
         updateStatLocally: (e) => {
             dispatch(updateStatSuccess(ownProps.stat, e.target.value));
