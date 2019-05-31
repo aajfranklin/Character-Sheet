b2f1f261302433077ee5d74c4fc114a4fe2a3af7
aajfranklin <aajfranklin@gmail.com>
2019-05-31 10:29:37 +0100

    blur Stat component on enter key press, causing submit

--- src/App/components/Stat/Stat.js
+++ src/App/components/Stat/Stat.js
@@ -4,9 +4,14 @@ import { INVALID_STAT } from '../Error/ErrorTypes';
 import { connect } from 'react-redux';
 
 export function Stat({beingEdited, cacheStat, handleBlur, stat, stats, updateStatLocally}) {
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') document.activeElement.blur();
+    };
+
     return (
         stat === beingEdited ?
-            <textarea value={stats[stat]} onChange={updateStatLocally} onBlur={handleBlur}/>
+            <textarea value={stats[stat]} onChange={updateStatLocally} onBlur={handleBlur} onKeyDown={handleKeyDown}/>
             : <span onClick={cacheStat}>{stats[stat]}</span>
     )
 }
@@ -15,18 +20,18 @@ function mapStateToProps(state) {
     return {
         stats: state.app.stats,
         beingEdited: state.app.statCache.stat
-
     };
 }
 
 function mapDispatchToProps(dispatch, ownProps) {
     return {
         handleBlur: (e) => {
-            if (isNaN(e.target.value)) {
+            const value = e.target.value;
+            if (isNaN(value)|| value === '') {
                 dispatch(revertStat(ownProps.stat));
                 dispatch(toggleShowError(INVALID_STAT));
             } else {
-                dispatch(updateStat(ownProps.stat, parseInt(e.target.value)));
+                dispatch(updateStat(ownProps.stat, parseInt(value)));
                 dispatch(cacheStat(''));
             }
         },
